84e3b6afae47081e32e6c2c8927e0038e362c1a8
pedros18 <badrofcb2004@gmail.com>
2024-04-27 20:38:07 +0100

    3rd commit XD

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const https = require("https");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function(req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Algiers&appid=7e1658c5c8d11674c3e7484f3bdf7300&units=metric";
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const city = req.body.cityName;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7e1658c5c8d11674c3e7484f3bdf7300&units=metric";
 
     https.get(url, function(response) {
         console.log(response.statusCode);
@@ -12,12 +19,12 @@ app.get("/", function(req, res) {
             const weatherdata = JSON.parse(data);
             console.log(weatherdata);
             const val = weatherdata.main.temp;
-            const icon = "http://openweathermap.org/img/wn/" + weatherdata.weather[0].icon + "@2x.png"
+            const icon = "http://openweathermap.org/img/wn/" + weatherdata.weather[0].icon + "@2x.png";
             const desc = weatherdata.weather[0].description;
             console.log(desc);
             res.write("<div style='text-align: center;'>");
             res.write("<h2 style='font-size: 24px; font-family: arial;'>The weather is " + desc + "</h2>");
-            res.write("<h1 style='font-size: 48px;font-family: arial;'>The temperature in Algiers is °" + val + "C</h1>");
+            res.write("<h1 style='font-size: 48px;font-family: arial;'>The temperature in " + city + " is " + val + " °C</h1>");
             res.write("<img src='" + icon + "' style='width: 300px; height: 300px;'>");
             res.write("</div>");
             res.send();
